e89a90ff9db3da584266974aa924be37137fddbc
Asitha Seneviratne <asisenevi@gmail.com>
2024-07-14 13:08:34 +0530

    Pie chart Component

---
+++ src/Components/Home/Sections/Chart/Partials/Pie.tsx
@@ -0,0 +1,87 @@
+import React, { memo, useEffect } from 'react';
+import HighCharts from 'highcharts';
+import Exporting from 'highcharts/modules/exporting';
+
+// To enable context menu
+Exporting(HighCharts);
+
+type PieChartPropType = {
+  series: HighCharts.SeriesPieOptions[],
+  title?: HighCharts.TitleOptions,
+  subTitle?: HighCharts.SubtitleOptions,
+  plotOptions?:  HighCharts.PlotOptions,
+};
+
+const defaultTitleOptions: HighCharts.TitleOptions = {
+  text: 'Product Categories',
+  align: 'left',
+};
+
+const defaultSubtitleOptions: HighCharts.SubtitleOptions = {
+  text: 'All available product categories',
+  align: 'left',
+};
+
+const defaultPlotOptions: HighCharts.PlotOptions = {
+  series: {
+    allowPointSelect: true,
+    cursor: 'pointer',
+  }
+};
+
+const exporting: HighCharts.ExportingOptions = {
+  buttons: {
+    contextButton: {
+      menuItems: [
+        'viewFullscreen',
+        'printChart',
+        'downloadPNG',
+        'downloadJPEG',
+        'downloadPDF',
+        'downloadSVG',
+      ],
+    },
+  },
+};
+
+const PieChart = (props: PieChartPropType) => {
+  const {
+    title,
+    subTitle,
+    plotOptions,
+    series,
+  } = props
+  useEffect(() => {
+    chartRender();
+  },[props]);
+
+  const chartRender = () => {
+    HighCharts.chart({
+      chart: {
+        type: 'pie',
+        renderTo: 'pie-chart-container'
+      },
+      title: {
+        ...defaultTitleOptions,
+        ...title
+      },
+      subtitle: {
+        ...defaultSubtitleOptions,
+        ...subTitle
+      },
+      plotOptions: {
+        ...defaultPlotOptions,
+        ...plotOptions,
+      },
+      series,
+      exporting,
+    });
+  };
+
+  return (
+    <div id="pie-chart-container" />
+  );
+};
+
+
+export default memo(PieChart)
--- src/Components/Home/Sections/Chart/index.tsx
+++ src/Components/Home/Sections/Chart/index.tsx
@@ -1,5 +1,112 @@
-import React from 'react';
+import React, { memo } from 'react';
+import { createStructuredSelector } from 'reselect';
+import HighCharts from 'highcharts';
+import { Grid } from '@mui/material';
+import { useSelector } from 'react-redux';
+import {
+  makeCategoriesLoading,
+  makeCategoryData,
+  makeProductsLoading,
+  makeProductData,
+  makeSelectedProducts,
+} from '../../../../Selectors';
+import { makeSelectedCategory } from '../../../../Selectors';
+import PieChart from './Partials/Pie';
+import Spinner from '../../../Common/Spinner';
+import { CategoryType, ProductType } from '../../../../CustomTypes';
 
-const ChartSection = () => <h2>Chart Section</h2>;
+const series: HighCharts.SeriesPieOptions[] = [{
+  name: 'Count',
+  type: 'pie',
+  data: [
+      {
+          name: 'Water',
+          y: 55.02
+      },
+      {
+          name: 'Fat',
+          y: 26.71
+      },
+      {
+          name: 'Carbohydrates',
+          y: 1.09
+      },
+      {
+          name: 'Protein',
+          y: 15.5
+      },
+      {
+          name: 'Ash',
+          y: 1.68
+      }
+  ]
+}];
 
-export default ChartSection;
+const stateSelector = createStructuredSelector({
+  categories: state => makeCategoryData(state),
+  categoriesLoading: (state) => makeCategoriesLoading(state),
+  selectedCategory: (state) => makeSelectedCategory(state),
+  products: (state) => makeProductData(state),
+  productsLoading: (state) => makeProductsLoading(state),
+  selectedProducts: (state) => makeSelectedProducts(state),
+});
+
+const ChartSection = () => {
+  const {
+    categories,
+    categoriesLoading,
+    selectedCategory,
+    products,
+    productsLoading,
+    selectedProducts,
+  } = useSelector(stateSelector);
+  
+
+  const getPieChartSeriesData = (): HighCharts.SeriesPieOptions[] => {
+    if (selectedCategory) {
+      return [{ 
+        name: 'Price',
+        type: 'pie',
+        data: products.map((product: ProductType) => (
+          { name: product.title, y: product.price  }
+        )),
+      }];
+    }
+    return [{ 
+      name: 'Category',
+      type: 'pie',
+      data: categories.map((category: CategoryType) => (
+        { name: category.name, y: 1 }
+      )),
+    }];
+  };
+
+  const renderChart = ()  => {
+    let title = {};
+    let subTitle = {};
+
+    if (selectedCategory) {
+      const category: any = categories.find((category: CategoryType) => category.slug === selectedCategory);
+      const categoryName = category ? category.name : '';
+
+      title = {
+        text: `Products of the selected category - ${categoryName}`
+      };
+      subTitle = {
+        text: `Price details of the selected category products`
+      }
+    }
+
+    return <PieChart series={getPieChartSeriesData()} title={title} subTitle={subTitle}/>
+  };
+
+  return (
+    <Spinner backdropProps={{ open: categoriesLoading || productsLoading }}>
+      <Grid container className='chart-container'>
+        {renderChart()}
+      </Grid>
+    </Spinner>
+  );
+};
+
+export default memo(ChartSection);
